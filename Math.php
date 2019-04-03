db295ca2e450ae4b2dc186fbea2436894e11bffb
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2019-04-03 14:43:52 -0300

    update value

--- Math.php
+++ Math.php
@@ -1,29 +1,29 @@
 <?php
 class Math{
 		
-	public static function factorial(int $number): array
-	{
-		if($number > 20){
-			throw new InvalidArgumentException('Value must be less than 20');
-		}
-		
-		return self::generateFactorial($number, [
-			'numbers' => null,
-			'total' => 1
-		]);		
-	}
+    public static function factorial(int $number): array
+    {
+        if($number > 20){
+            throw new InvalidArgumentException('Value must be less than 20');
+        }
+
+        return self::generateFactorial($number, [
+            'numbers' => null,
+            'total' => 1
+        ]);
+    }
   
-	private static function generateFactorial(int $number, array $factorial): array
-	{
-		if($number <= 1){
-			$factorial['numbers'] .= '1';
-			return $factorial;
-		}
-		
-		$factorial['numbers'] .= $number. ' x ';
-		$factorial['total'] *= $number;
-		
-		return self::generateFactorial($number - 1, $factorial);
-	}
+    private static function generateFactorial(int $number, array $factorial): array
+    {
+        if($number <= 1){
+            $factorial['numbers'] .= '1';
+            return $factorial;
+        }
+
+        $factorial['numbers'] .= $number. ' x ';
+        $factorial['total'] *= $number;
+
+        return self::generateFactorial($number - 1, $factorial);
+    }
 }
-print_r(Math::factorial(20));
+print_r(Math::factorial(10));
